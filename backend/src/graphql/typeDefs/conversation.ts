0cba21189fefe4933ca025e364f676d8fb91df65
hazadus <hazadus7@gmail.com>
2023-09-10 01:52:38 +0300

    dev: Updated typedefs on the backend.

--- backend/src/graphql/typeDefs/conversation.ts
+++ backend/src/graphql/typeDefs/conversation.ts
@@ -1,12 +1,33 @@
 import { gql } from "apollo-server-core";
 
 const typeDefs = gql`
+  scalar Date
+
   type Mutation {
     createConversation(participantIds: [String]): CreateConversationResponse
   }
 
   type CreateConversationResponse {
     conversationId: String
+  }
+
+  type Query {
+    # Get all conversations of the user
+    conversations: [Conversation]
+  }
+
+  type Conversation {
+    id: String
+    # latestMessage: Message
+    participants: [Participant]
+    createdAt: Date
+    updatedAt: Date
+  }
+
+  type Participant {
+    id: String
+    user: User
+    hasSeenLatestMessage: Boolean
   }
 `;
 
--- backend/src/graphql/typeDefs/user.ts
+++ backend/src/graphql/typeDefs/user.ts
@@ -1,14 +1,23 @@
 import { gql } from "apollo-server-core";
 
 const typeDefs = gql`
-  type SearchedUser {
+  type User {
     id: String
+    name: String
     username: String
+    email: String
+    emailVerified: Boolean
     image: String
   }
 
   type Query {
     searchUsers(username: String): [SearchedUser]
+  }
+
+  type SearchedUser {
+    id: String
+    username: String
+    image: String
   }
 
   type Mutation {
