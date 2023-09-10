4d35090e1138d5a9d07f7148d24e9f74e0e622d1
hazadus <hazadus7@gmail.com>
2023-09-10 19:17:23 +0300

    dev: Frontend configured to use websockets.

--- frontend/src/graphql/apollo-client.ts
+++ frontend/src/graphql/apollo-client.ts
@@ -1,11 +1,36 @@
-import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
+import { ApolloClient, HttpLink, InMemoryCache, split } from "@apollo/client";
+import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
+import { getMainDefinition } from "@apollo/client/utilities";
+import { createClient } from "graphql-ws";
 
 const httpLink = new HttpLink({
   uri: "http://localhost:4000/graphql",
   credentials: "include",
 });
 
+// Ensure these code blocks are running in the browser:
+const wsLink =
+  typeof window !== "undefined"
+    ? new GraphQLWsLink(
+        createClient({
+          url: "ws://localhost:4000/graphql/subscriptions",
+        }),
+      )
+    : null;
+
+const link =
+  typeof window !== "undefined" && wsLink != null
+    ? split(
+        ({ query }) => {
+          const definition = getMainDefinition(query);
+          return definition.kind === "OperationDefinition" && definition.operation === "subscription";
+        },
+        wsLink,
+        httpLink,
+      )
+    : httpLink;
+
 export const client = new ApolloClient({
-  link: httpLink,
+  link: link,
   cache: new InMemoryCache(),
 });
