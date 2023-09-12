a693aec217811077f6edd23a4a274f79d21d7fb1
hazadus <hazadus7@gmail.com>
2023-09-13 00:52:27 +0300

    docs: Added docstrings in backend GraphQL schemas.

--- backend/src/graphql/typeDefs/user.ts
+++ backend/src/graphql/typeDefs/user.ts
@@ -11,16 +11,31 @@ const typeDefs = gql`
   }
 
   type Query {
+    """
+    Search user by part of his username.
+    """
     searchUsers(username: String): [SearchedUser]
   }
 
+  """
+  User info returned by searchUsers
+  """
   type SearchedUser {
     id: String
+    """
+    Username chosen by user in our app.
+    """
     username: String
+    """
+    Image URL from user's Google account.
+    """
     image: String
   }
 
   type Mutation {
+    """
+    Set username for currently signed in user.
+    """
     createUsername(username: String): CreateUsernameResponse
   }
 
