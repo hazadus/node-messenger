f8c9a0aac356b27ddb21ae729de0b0a4e56ecd96
hazadus <hazadus7@gmail.com>
2023-09-14 18:36:27 +0300

    fix: Fixed wrong property name.

--- backend/src/helpers.ts
+++ backend/src/helpers.ts
@@ -4,5 +4,5 @@ export const userIsConversationParticipant = (
   participants: Array<ParticipantPopulated>,
   userId: string,
 ): boolean => {
-  return !!participants.find((participant) => participant.id === userId);
+  return !!participants.find((participant) => participant.userId === userId);
 };
