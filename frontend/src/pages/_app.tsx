35848570224729ac588562fd4119afe2c5406ab5
hazadus <hazadus7@gmail.com>
2023-09-22 09:53:06 +0300

    config: Added tracking code for stats.hazadus.ru.

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -4,18 +4,28 @@ import { ApolloProvider } from "@apollo/client";
 import { ChakraProvider } from "@chakra-ui/react";
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import { Toaster } from "react-hot-toast";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <ApolloProvider client={client}>
-      <SessionProvider session={session}>
-        <ChakraProvider theme={theme}>
-          <Component {...pageProps} />
-          <Toaster />
-        </ChakraProvider>
-      </SessionProvider>
-    </ApolloProvider>
+    <>
+      <Head key="_app.tsx">
+        <script
+          async
+          src="https://stats.hazadus.ru/script.js"
+          data-website-id="8101efef-cafb-4338-9b58-e6907809f14f"
+        ></script>
+      </Head>
+      <ApolloProvider client={client}>
+        <SessionProvider session={session}>
+          <ChakraProvider theme={theme}>
+            <Component {...pageProps} />
+            <Toaster />
+          </ChakraProvider>
+        </SessionProvider>
+      </ApolloProvider>
+    </>
   );
 }
 
