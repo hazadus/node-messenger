93daee0b5fdbb84cfafd09ce0c66af4ce471d710
hazadus <hazadus7@gmail.com>
2023-09-22 08:39:57 +0300

    ui: Limit messages max width; increase padding in messages container.

--- frontend/src/Components/Chat/Feed/Messages/MessageItem.tsx
+++ frontend/src/Components/Chat/Feed/Messages/MessageItem.tsx
@@ -13,10 +13,7 @@ const MessageItem: React.FC<MessageItemProps> = ({ message, isSentBySignedInUser
     // Full message "line"
     <Flex justify={isSentBySignedInUser ? "right" : "left"}>
       {/* Avatar + Message */}
-      <Stack
-        maxWidth="60%"
-        direction="row"
-      >
+      <Stack direction="row">
         {/* User avatar */}
         {!isSentBySignedInUser && (
           <Avatar
@@ -28,6 +25,7 @@ const MessageItem: React.FC<MessageItemProps> = ({ message, isSentBySignedInUser
         {/* Message block */}
         <Stack
           minWidth="120px"
+          maxWidth={{ base: "80%", md: "464px" }}
           py={1}
           px={2}
           bgColor={isSentBySignedInUser ? "brand.100" : "whiteAlpha.300"}
